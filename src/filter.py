663a8b360071e4ba06f7c0b0fcd05030fa34cb73
garychn1841 <garychn1841@gmail.com>
2023-03-06 02:03:53 +0800

    change start to 45 and add same tickers filter

--- src/filter.py
+++ src/filter.py
@@ -49,32 +49,60 @@ def new_tickers(season_tickers):
 # price(ticker = the list of new tickers are add for indivdually season)
 # output the average wave 
 
-def price(ticker,date):
+def price(ticker,date,investor):
     aa=[]
     tol = 0
     ave = 0
     
     if ticker:
         for i in range(len(ticker)):
-            start = date + timedelta(days=1)
+            start = date + timedelta(days=45)
             end = start + timedelta(days=15)
             a = stock.download(str(ticker[i]),start=start,end=end)
 
             min = a['Adj Close'].min()
             max = a['Adj Close'].max()
-            print((max-min)/min)
+            print(print(investor,ticker[i]))
 
             tol = tol + (max-min)/min if not a.empty else tol + 0 
             # aa.append(ticker[i]) if ((max-min)/min) >= 0.10 else None
-            print(str(ticker[i]))
-            print("accumulated:",tol)
+        #     print(str(ticker[i]))
+        #     print("accumulated:",tol)
 
-        print(ticker , len(ticker))
+        # print(ticker , len(ticker))
         ave = tol / len(ticker)
 
     return ave
 
+def same_tickers(season_tickers):
 
+
+    # print(season_tickers[6][season_tickers[6]['ticker'] == 'S'])
+
+    for x in season_tickers[1].ticker:
+        print(x)
+        print(season_tickers[0][season_tickers[0]['ticker'] == x])
+        print(season_tickers[1][season_tickers[1]['ticker'] == x]) 
+        print(season_tickers[0][season_tickers[0]['ticker'] == x].head(1))
+        print(season_tickers[1][season_tickers[1]['ticker'] == x].head(1))  
+        # if x in season_tickers[0].ticker.values and season_tickers[0][season_tickers[0]['ticker'] == x].shares.values == season_tickers[1][season_tickers[1]['ticker'] == x].shares.values:
+            
+        #     print('ok')
+
+    x1 = [x for x in season_tickers[1].ticker if x in season_tickers[0].ticker.values and season_tickers[0][season_tickers[0]['ticker'] == x].head(1).shares.values == season_tickers[1][season_tickers[1]['ticker'] == x].head(1).shares.values]
+    x2 = [x for x in season_tickers[2].ticker if x in season_tickers[1].ticker.values and season_tickers[2][season_tickers[2]['ticker'] == x].head(1).shares.values == season_tickers[1][season_tickers[1]['ticker'] == x].head(1).shares.values] 
+    x3 = [x for x in season_tickers[3].ticker if x in season_tickers[2].ticker.values and season_tickers[3][season_tickers[3]['ticker'] == x].head(1).shares.values == season_tickers[2][season_tickers[2]['ticker'] == x].head(1).shares.values]
+    x4 = [x for x in season_tickers[4].ticker if x in season_tickers[3].ticker.values and season_tickers[4][season_tickers[4]['ticker'] == x].head(1).shares.values == season_tickers[3][season_tickers[3]['ticker'] == x].head(1).shares.values]
+    x5 = [x for x in season_tickers[5].ticker if x in season_tickers[4].ticker.values and season_tickers[5][season_tickers[5]['ticker'] == x].head(1).shares.values == season_tickers[4][season_tickers[4]['ticker'] == x].head(1).shares.values]
+    x6 = [x for x in season_tickers[6].ticker if x in season_tickers[5].ticker.values and season_tickers[6][season_tickers[6]['ticker'] == x].head(1).shares.values == season_tickers[5][season_tickers[5]['ticker'] == x].head(1).shares.values]
+    x7 = [x for x in season_tickers[7].ticker if x in season_tickers[6].ticker.values and season_tickers[7][season_tickers[7]['ticker'] == x].head(1).shares.values == season_tickers[6][season_tickers[6]['ticker'] == x].head(1).shares.values]
+    x8 = [x for x in season_tickers[8].ticker if x in season_tickers[7].ticker.values and season_tickers[8][season_tickers[8]['ticker'] == x].head(1).shares.values == season_tickers[7][season_tickers[7]['ticker'] == x].head(1).shares.values]
+    x9 = [x for x in season_tickers[9].ticker if x in season_tickers[8].ticker.values and season_tickers[9][season_tickers[9]['ticker'] == x].head(1).shares.values == season_tickers[8][season_tickers[8]['ticker'] == x].head(1).shares.values]
+    x10 = [x for x in season_tickers[10].ticker if x in season_tickers[9].ticker.values and season_tickers[10][season_tickers[10]['ticker'] == x].head(1).shares.values == season_tickers[9][season_tickers[9]['ticker'] == x].head(1).shares.values]
+    x11 = [x for x in season_tickers[11].ticker if x in season_tickers[10].ticker.values and season_tickers[11][season_tickers[11]['ticker'] == x].head(1).shares.values == season_tickers[10][season_tickers[10]['ticker'] == x].head(1).shares.values]
+
+
+    return x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11
 
 
 
@@ -84,35 +112,53 @@ if __name__ == '__main__':
     average_percent = []
     ave_plt = []
 
-    df = pd.read_csv('D:/PYTHON/Dataset/Growin/row_data.csv')
+    df = pd.read_csv('/Users/gary/Documents/Python/data/growin/row_data.csv')
     df = df.drop(df[df['ticker'] == 'GTLS PR B'].index)
+    df = df.drop(df[df['ticker'] == 'S'].index)
+    # unchangelist(df)
 
     season = df.date.unique()
     season = [datetime.strptime(x,'%Y-%m-%d') for x in season]
     investor = df.investor.unique()
+    print(investor)
+     
+
+    
 
     # a = stock.download('GTLS PR B',start='2023-01-01',end='2023-03-04')
     # print(a)
-    for name in ['Janus Henderson Investors']:
+    for name in investor[1:5]:
     
         df_season = sensonzier(df,name)
+        print(df_season)
         new_ticker_list = new_tickers(df_season)
+        same = same_tickers(df_season)
+
         tol_ave = 0 
+        tol_ave_same = 0
 
 
         for index,element in enumerate(new_ticker_list):
-            ave = price(element,season[1+index])
+            ave = price(element,season[1+index],name)
             tol_ave = tol_ave + ave
             ave_plt.append(ave)
+
+        for index,element in enumerate(same):
+            same_ave = price(element,season[1+index],name)
+            tol_ave_same = tol_ave + same_ave
+            ave_plt.append(same_ave)
+
         tol_average = tol_ave/len(new_ticker_list)
-        average_dict = {name : tol_ave/len(new_ticker_list)}
+        same_average = tol_ave_same/len(same)
+        y = (tol_average,same_average)
+        average_dict = {name : y}
+        print(average_dict)
+    # print(average_dict)
 
 
-    print(average_dict)
-
-    x = np.arange(1,12)
-    plt.bar(x,ave_plt)
-    plt.xlabel('season')
-    plt.ylabel('percentage of wave')
-    plt.savefig('plot.png')
-    plt.show()
+    # x = np.arange(1,12)
+    # plt.bar(x,ave_plt)
+    # plt.xlabel('season')
+    # plt.ylabel('percentage of wave')
+    # plt.savefig('plot.png')
+    # plt.show()
