7cd3468d48b6ca3e95786ffe4a534d13da5a8fb5
garychn1841 <garychn1841@gmail.com>
2023-03-13 15:31:44 +0800

    modify to_csv name

--- src/styleclusting.py
+++ src/styleclusting.py
@@ -3,7 +3,9 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('/Users/gary/Documents/Python/data/growin/styleweight/StyleWeight_full.csv')
+df['Unnamed: 0.1'] = df['Unnamed: 0.1'].map(lambda x:x.lstrip('df_'))
 season = df['Unnamed: 0.1'].unique()
+print(season)
 for i  in range(3,7):
     for period in season:
 
@@ -14,4 +16,4 @@ for i  in range(3,7):
 
         kmeans = KMeans(n_clusters=i,random_state=1,n_init="auto").fit(x)
         df1['label'] = kmeans.labels_
-        df1.to_csv('%s_n%s.csv'%(period,i),index = False)
+        df1.to_csv('clusting_result/%s_n%s.csv'%(period,i),index = False)
